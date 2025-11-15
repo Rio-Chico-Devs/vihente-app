180a904bf5b219795241335d2c5d1e9038a806cb
bruno <antoniovbruno@gmail.com>
2025-11-15 19:06:01 +0100

    feat: implement self-hosted fonts for GDPR compliance

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,9 @@
+import '@fontsource/share-tech-mono/400.css';
+import '@fontsource/orbitron/400.css';
+import '@fontsource/orbitron/700.css';
+import '@fontsource/orbitron/900.css';
+import '@fontsource/electrolize/400.css';
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
